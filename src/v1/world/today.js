a2fb0c92fc45e33b49952e8e9cddd9d55dd8aced
thienmta <thienmta.py@gmail.com>
2020-09-29 23:05:29 +0700

    add today yesterday last7day

---
+++ src/v1/world/today.js
@@ -0,0 +1,8 @@
+var today = {
+  "Confirmed": 298085,
+  "Cumulative Confirmed": 31798308,
+  "Deaths": 5918,
+  "Cumulative Deaths": 973653
+}
+
+module.exports = today;
---
+++ src/v1/world/yesterday.js
@@ -0,0 +1,8 @@
+var yesterday = {
+  "Confirmed": 252899,
+  "Cumulative Confirmed": 31500223,
+  "Deaths": 4569,
+  "Cumulative Deaths": 967735
+}
+
+module.exports = yesterday;
