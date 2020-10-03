11be45541d1bc58990d5220f26b089ec305b94e7
Phạm Tính <phamtinh@Phams-MacBook-Pro.local>
2020-10-04 00:04:31 +0700

    add api table

---
+++ src/v1/table/flight.js
@@ -0,0 +1,20 @@
+const flight = [
+  {
+    type: 'Tổng số giám sát',
+    data: [
+      { 'title': 'Tổng số chuyến bay dám sát từ đầu dịch', 'amount': 292},
+      { 'title': 'Tổng số chuyến bay giám sát có ca dương tính', 'amount': 138},
+      { 'title': 'Tổng số ca dương tính', 'amount': 200},
+    ]
+  },
+  {
+    type: 'Bảo trợ công dân',
+    data: [
+      { 'title': 'Tổng số chuyến bay bảo trợ công dân', 'amount': 292},
+      { 'title': 'Tổng số chuyến bay bảy bảo trợ có ca dương tính', 'amount': 138},
+      { 'title': 'Tổng số ca dương tính', 'amount': 500 },
+      { 'title': 'Tổng số công dân, ngoại dao được bảo vệ', 'amount': 500 },
+    ]
+  }
+]
+module.exports = flight;
