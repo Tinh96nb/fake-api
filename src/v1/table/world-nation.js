05a55cddfeed380ace03f24efce81a0d52e20444
tinhpd1 <tinhpd1@vnext.com.vn>
2020-10-09 16:56:22 +0700

    add api world table

---
+++ src/v1/table/world-nation.js
@@ -0,0 +1,33 @@
+const world = [
+    { nation: 'Hoa Kỳ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Brazil', all: 300, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Nga', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Ấn Độ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Anh', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Hoa Kỳ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Hoa Kỳ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Hoa Kỳ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Brazil', all: 300, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Nga', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Ấn Độ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Anh', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Hoa Kỳ', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: 'Hoa Kỳ', all: 500, infecting: 233, dead: 12, cured: 123 },
+  ];
+const times = [
+    { nation: '12-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '13-02-2020', all: 300, infecting: 233, dead: 12, cured: 123 },
+    { nation: '14-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '15-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '16-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '17-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '18-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '19-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '20-02-2020', all: 300, infecting: 233, dead: 12, cured: 123 },
+    { nation: '21-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '13-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '13-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '18-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+    { nation: '18-02-2020', all: 500, infecting: 233, dead: 12, cured: 123 },
+  ]
+module.exports = world;
